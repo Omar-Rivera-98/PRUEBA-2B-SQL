55a1f59960f966a0eb92d7cb9e2db1052bd3df47
Omar <20240600@ricaldone.edu.sv>
2025-06-23 12:11:55 -0600

    Listo

--- Prueba2B-Omar.sql
+++ Prueba2B-Omar.sql
@@ -1,21 +1,22 @@
+/*Script Base de Datos*/
 CREATE DATABASE ejemplo2B
 GO
 USE ejemplo2B
 GO
-
+/*Script Creacion de tablas*/
 CREATE TABLE Especialidad(
 idEspecialidad int identity (1,1) PRIMARY KEY,
 nombreEspecialidad VARCHAR (25) not null);
 
 GO
-
+/*Script Creacion de tablas*/
 CREATE TABLE Grado(
 idGrado int identity (1,1) primary key,
 nombreGrado VARCHAR (25) not null);
 
 GO
 
-
+/*Script Creacion de tablas*/
 CREATE TABLE Estudiante(
 idEstudiante int identity (100,1) primary key,
 nombreEstudiante VARCHAR (25) not null,
@@ -26,32 +27,39 @@ id_Especialidad int,
 id_Grado int,
 foreign key (id_Grado) references Grado (idGrado),
 foreign key (id_Especialidad) references Especialidad(idEspecialidad));
-
+/*Seleccion*/
 select * from Grado
 select * from Estudiante
 select * from Especialidad
-
+/*Inserccion de datos*/
 insert into Especialidad values
 ('Software'),
-('Diseño'),
+('DiseÃ±o'),
 ('Arquitectura'),
 ('Contabilidad'),
 ('Electromecanica'),
 ('Renovables'),
 ('Automotriz'),
 ('Electronica')
-
+/*Inserccion de datos*/
 insert into Grado values
-('1º Año'),
-('2º Año'),
-('3º Año')
-
+('1Âº AÃ±o'),
+('2Âº AÃ±o'),
+('3Âº AÃ±o')
+/*Inserccion de datos*/
 insert into Estudiante values
 ('Omar Ernesto','Rivera Hernandez','20240600',16,1,1),
 ('Luis Edgardo','Soriano Rivas','20220011',16,1,1),
 ('Rosemberg Astul','Ramos','20240100',16,1,1),
 ('Chelsea Mariana','Mejia Martinez','20241012',16,1,1)
-
-Exec sp_rename 'Estudiante.nombre', 'nombreEstuiante','COLUMN';
+/*Renombramiento de columnas*/
+Exec sp_rename 'Estudiante.nombre', 'nombreEstudiante','COLUMN';
 Exec sp_rename 'Estudiante.Grado', 'nombreGrado','COLUMN';
-
+/*Seleccion*/
+SELECT nombreEstudiante AS Nombre_Estudiante,apellido,edad,
+nombreEspecialidad AS Especialidad, nombreGrado AS Grado
+from Estudiante AS E
+JOIN
+Especialidad ON E.id_Especialidad = Especialidad.idEspecialidad
+JOIN 
+Grado AS G ON E.id_Grado=G.idGrado
